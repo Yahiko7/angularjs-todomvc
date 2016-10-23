b89f455a2481c7b6f5bce9d30990e7a6ea67c9b5
unknown <>
2016-10-23 11:12:41 +0800

    做了angular-todomvc的准备工作,添加了$scope.text和$scope.workList任务对象

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
-(function (window) {
+(function (angular) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
-
-})(window);
+	var todoApp = angular.module('todoApp',[]);
+	
+	todoApp.controller('todoController',['$scope',function($scope){
+		
+		$scope.text = '';
+		
+		$scope.workList = [{
+	      id: 0.123,
+	      text: '学习',
+	      completed: false
+	    }, {
+	      id: 0.22,
+	      text: '睡觉',
+	      completed: false
+	    }, {
+	      id: 0.232,
+	      text: '打豆豆',
+	      completed: true
+	    },];
+		
+	}])
+	
+})(angular);
